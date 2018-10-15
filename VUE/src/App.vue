f14256d6feade737a63f920367337a71f8dbb0a9
IZAYA <1321819852@qq.com>
2018-10-15 10:38:05 +0800

    edit

---
+++ VUE/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body,h1,h2,h3,h4,h5,h6,p,ul,li{padding: 0;margin: 0;}
+  ul,li{list-style: none;}
+  a{text-decoration: none;}
+</style>
---
+++ VUE/src/components/Test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>{{asd}}</li>
+      <li>{{5+5}}</li>
+      <li><router-link to="/">回首页</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'This is test',
+      asd: 'assdaadfadfa'
+    }
+  }
+}
+console.log($('.hello').eq(0).height());
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
