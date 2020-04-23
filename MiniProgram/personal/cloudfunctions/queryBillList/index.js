a1dee12b5714fd48ac8eed3aedbdedaa53bb792c
Izaya <1321819852@qq.com>
2020-04-23 17:31:40 +0800

    2020.4023

--- MiniProgram/personal/cloudfunctions/queryBillList/index.js
+++ MiniProgram/personal/cloudfunctions/queryBillList/index.js
@@ -4,30 +4,113 @@ cloud.init()
 const db = cloud.database()
 const _ = db.command
 // 云函数入口函数
-exports.main = async (event, context) => {
-  const { pageSize, pageNum, dateTime, payType, costCategories, consumptionType } = event
-  const { OPENID } = cloud.getWXContext()
-  const { startTime, endTime } = getRange(dateTime)
-  let queryOption = { 'userInfo.openId': OPENID }
+exports.main = async(event, context) => {
+  const {
+    pageSize,
+    pageNum,
+    dateTime,
+    dateType,
+    payType,
+    costCategories,
+    consumptionType
+  } = event
+  const {
+    OPENID
+  } = cloud.getWXContext()
+  const {
+    startTime,
+    endTime
+  } = getRange(dateTime, dateType)
+  let queryOption = {
+    'userInfo.openId': OPENID
+  }
   if (startTime) queryOption.date = _.and(_.gt(startTime), _.lt(endTime))
   if (payType) queryOption.payType = payType
   if (costCategories) queryOption.costCategories = costCategories
   if (consumptionType) queryOption.consumptionType = consumptionType
-  const res = await db.collection('bill').where(queryOption).limit(pageSize).skip(pageNum * pageSize).get()
-
-  return res.data
-}
-
-function getRange(dateTime) {
-  if(!dateTime) return {}
-  let year = new Date(dateTime).getFullYear()
-  let month = new Date(dateTime).getMonth()
-  if (month === 0) {
-    month = 12;
-    year = year - 1;
+  const {
+    total
+  } = await db.collection('bill').where(queryOption).count()
+  if (total > pageNum * pageSize) {
+    const {
+      data
+    } = await db.collection('bill').where(queryOption).limit(pageSize).skip(pageNum * pageSize).get()
+    const payTypeList = await queryPayType()
+    const consumptionList = await queryConsumptionType()
+    const categoriesList = await queryCategories()
+    data.forEach(item => {
+      item.payTypeName = payTypeList.filter(fitem => item.payType === fitem.id)[0].name
+      item.consumptionName = consumptionList.filter(fitem => item.consumptionType === fitem.id)[0].name
+      item.costCategoriesName = categoriesList.filter(fitem => item.costCategories === fitem.id)[0].name
+    })
+    return {
+      list:data,
+      pageNum,
+      pageSize,
+      total
+    }
   }
   return {
-    startTime: new Date(year, month).getTime(),
-    endTime: new Date(year, month + 1 > 12 ? 1 : month + 1, 0).getTime()
+    pageNum,
+    pageSize,
+    total
   }
+}
+
+function getRange(dateTime, dateType = 'month') {
+  if (!dateTime) return {}
+  let year = new Date(dateTime).getFullYear()
+  let month = new Date(dateTime).getMonth()
+  switch (dateType) {
+    case 'month':
+      let endTime = 0
+      if (month + 1 === 12) {
+        endTime = new Date(year + 1, 1, 0).getTime()
+      } else {
+        endTime = new Date(year, month + 1, 0).getTime()
+      }
+      return {
+        startTime: new Date(year, month).getTime(),
+        endTime,
+      }
+    case 'year':
+      return {
+        startTime: new Date(year, 0).getTime(),
+        endTime: new Date(year + 1, 0, 0).getTime()
+      }
+    default:
+      return {}
+  }
+
+}
+async function queryPayType() {
+  const res = await cloud.callFunction({
+    name: 'queryPayType',
+    data: {},
+  })
+  let list = []
+  res.result.forEach(item => {
+    item = item.children.map(citem => {
+      return {
+        id: citem.id,
+        name: `${item.name}/${citem.name}`
+      }
+    })
+    list = list.concat(item)
+  })
+  return list
+}
+async function queryConsumptionType() {
+  const res = await cloud.callFunction({
+    name: 'queryConsumption',
+    data: {},
+  })
+  return res.result
+}
+async function queryCategories() {
+  const res = await cloud.callFunction({
+    name: 'queryCategories',
+    data: {},
+  })
+  return res.result
 }
--- MiniProgram/personal/miniprogram/pages/billList/billList.js
+++ MiniProgram/personal/miniprogram/pages/billList/billList.js
@@ -1,14 +1,16 @@
 // miniprogram/pages/billList/billList.js
-const {
-  cloudRequest
-} = require('../../utils/util.js')
+const {cloudRequest} = require('../../utils/util.js')
 Page({
   data: {
     sendData: {
       pageSize: 10,
       pageNum: 0,
       dateTime: new Date().getTime()
-    }
+    },
+    total: 0,
+    billList: [],
+    TabCur: 0,
+    tabList: ['清单列表', '图表']
   },
   onLoad: function(options) {},
   onReady: function() {
@@ -24,7 +26,53 @@ Page({
       name: 'queryBillList',
       data: this.data.sendData
     }).then(res => {
+      const { pageNum, pageSize, total, list } = res.result
+      const billList = list.map(item => {
+        item.date = new Date(item.date).Format('yyyy-MM-dd')
+        item.money = parseFloat(item.money).toFixed(2)
+        return item
+      })
+      this.setData({
+        billList,
+        total,
+        'sendData.pageNum': pageNum,
+        'sendData.pageSize': pageSize
+      })
       console.log(res)
+    })
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id
+    })
+  },
+  // ListTouch触摸开始
+  ListTouchStart(e) {
+    this.setData({
+      ListTouchStart: e.touches[0].pageX
+    })
+  },
+
+  // ListTouch计算方向
+  ListTouchMove(e) {
+    this.setData({
+      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // ListTouch计算滚动
+  ListTouchEnd(e) {
+    if (this.data.ListTouchDirection == 'left') {
+      this.setData({
+        modalName: e.currentTarget.dataset.target
+      })
+    } else {
+      this.setData({
+        modalName: null
+      })
+    }
+    this.setData({
+      ListTouchDirection: null
     })
   }
 })
